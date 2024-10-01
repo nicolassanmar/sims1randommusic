7e62b52846798de70b08902dca82c040f9625c3d
Nicolas <nicolassanmar@gmail.com>
2024-09-30 23:38:42 -0300

    random sims 1 music select

--- README.md
+++ README.md
@@ -1,29 +1,6 @@
-# Create T3 App
+# Data source
 
-This is a [T3 Stack](https://create.t3.gg/) project bootstrapped with `create-t3-app`.
-
-## What's next? How do I make an app with this?
-
-We try to keep this project as simple as possible, so you can start with just the scaffolding we set up for you, and add additional things later when they become necessary.
-
-If you are not familiar with the different technologies used in this project, please refer to the respective docs. If you still are in the wind, please join our [Discord](https://t3.gg/discord) and ask for help.
-
-- [Next.js](https://nextjs.org)
-- [NextAuth.js](https://next-auth.js.org)
-- [Prisma](https://prisma.io)
-- [Drizzle](https://orm.drizzle.team)
-- [Tailwind CSS](https://tailwindcss.com)
-- [tRPC](https://trpc.io)
-
-## Learn More
-
-To learn more about the [T3 Stack](https://create.t3.gg/), take a look at the following resources:
-
-- [Documentation](https://create.t3.gg/)
-- [Learn the T3 Stack](https://create.t3.gg/en/faq#what-learning-resources-are-currently-available) — Check out these awesome tutorials
-
-You can check out the [create-t3-app GitHub repository](https://github.com/t3-oss/create-t3-app) — your feedback and contributions are welcome!
-
-## How do I deploy this?
-
-Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/vercel), [Netlify](https://create.t3.gg/en/deployment/netlify) and [Docker](https://create.t3.gg/en/deployment/docker) for more information.
+1. Copy table from https://sims.fandom.com/wiki/List_of_songs_in_The_Sims
+2. Regex replace `<div class="center"><div class="floatnone"><a href="/wiki/The_Sims.* title="(.*)"><img.*</div></div>` with `$1`
+3. Regex replace `<audio src="(.*)" width.*</audio>` with `$1`
+4. https://tableconvert.com/html-to-json
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,86 @@
-import Link from "next/link";
+"use client";
+// import src/data/in-game-music.json
+import inGameMusic from "~/data/in-game-music.json";
+import radioMusic from "~/data/radio-music.json";
+import { useEffect, useState } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "~/components/ui/accordion";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+import { Button } from "~/components/ui/button";
 
-import { LatestPost } from "~/app/_components/post";
-import { api, HydrateClient } from "~/trpc/server";
+const allMusic = [...inGameMusic, ...radioMusic];
 
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+export default function Home() {
+  const [randomMusic, setRandomMusic] = useState(
+    allMusic[Math.floor(Math.random() * allMusic.length)],
+  );
+  const [opened, setOpened] = useState("");
 
-  void api.post.getLatest.prefetch();
-
+  if (!randomMusic) {
+    // Should actualy never happen
+    return <div>Loading...</div>;
+  }
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-          </div>
+    <>
+      <Button
+        onClick={() => {
+          setOpened("");
+          setRandomMusic(allMusic[Math.floor(Math.random() * allMusic.length)]);
+        }}
+      >
+        Random Music
+      </Button>
 
-          <LatestPost />
-        </div>
-      </main>
-    </HydrateClient>
+      <audio src={randomMusic.Preview} controls style={{ margin: "100px" }} />
+
+      <a
+        href={randomMusic.Preview}
+        download
+        target="_blank"
+        referrerPolicy="no-referrer"
+        className="text-blue-500"
+      >
+        cansao
+      </a>
+
+      <Accordion type="single" collapsible value={opened}>
+        <AccordionItem value="item-1" onClick={() => setOpened("item-1")}>
+          <AccordionTrigger className="">Song</AccordionTrigger>
+          <AccordionContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px]">Title</TableHead>
+                  <TableHead>Artist</TableHead>
+                  <TableHead>Mode</TableHead>
+                  <TableHead>Station</TableHead>
+                  <TableHead>Pack</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell>{randomMusic.Title}</TableCell>
+                  <TableCell>{randomMusic.Artist}</TableCell>
+                  <TableCell>{randomMusic.Mode ?? "-"}</TableCell>
+                  <TableCell>{randomMusic.Station ?? "-"}</TableCell>
+                  <TableCell>{randomMusic.Pack}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </>
   );
 }
